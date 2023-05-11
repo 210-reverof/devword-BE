c93cf75b920cd4140c72791d1ef4268dda64c8f7
given-dragon <yijy001@gmail.com>
2023-05-12 00:41:38 +0900

    create user respository #3

---
+++ src/main/java/com/burnbunny/devword/domain/user/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.burnbunny.devword.domain.user.repository;
+
+import com.burnbunny.devword.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByNickname(String nickname);
+
+    Optional<User> findByRefreshToken(String refreshToken);
+
+    boolean existsByEmail(String email);
+    boolean existsByNickname(String nickname);
+}
