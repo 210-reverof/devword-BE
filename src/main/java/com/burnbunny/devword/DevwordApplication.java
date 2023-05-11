f3ce4ac431a6b4f8d2cb627ab54ebba62b01626b
given-dragon <yijy001@gmail.com>
2023-05-11 21:58:52 +0900

    init: add project

---
+++ src/main/java/com/burnbunny/devword/DevwordApplication.java
@@ -0,0 +1,13 @@
+package com.burnbunny.devword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevwordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevwordApplication.class, args);
+	}
+
+}
