ea4823d94eb08ba43216f5097b4b13d0a750b6ab
given-dragon <yijy001@gmail.com>
2023-05-12 01:40:19 +0900

    feat: user controller url통합 #3

--- src/main/java/com/burnbunny/devword/domain/user/controller/UserController.java
+++ src/main/java/com/burnbunny/devword/domain/user/controller/UserController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
--- src/main/java/com/burnbunny/devword/global/config/SecurityConfig.java
+++ src/main/java/com/burnbunny/devword/global/config/SecurityConfig.java
@@ -2,11 +2,37 @@ package com.burnbunny.devword.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .formLogin().disable()
+                .httpBasic().disable()
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+
+                .authorizeHttpRequests()
+                .requestMatchers("/", "/css/**", "/images/**", "/js/**", "/favicon.ico", "/h2-console/**").permitAll()
+                .requestMatchers("/user/sign-up", "user/check/**").permitAll()
+                .anyRequest().authenticated();
+
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
