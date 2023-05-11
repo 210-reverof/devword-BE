4b7549a9ec12e6e6c0bca4c2d5842594d3fe86ad
given-dragon <yijy001@gmail.com>
2023-05-12 00:54:48 +0900

    create user service #3

---
+++ src/main/java/com/burnbunny/devword/domain/user/service/UserService.java
@@ -0,0 +1,38 @@
+package com.burnbunny.devword.domain.user.service;
+
+import com.burnbunny.devword.domain.user.Role;
+import com.burnbunny.devword.domain.user.User;
+import com.burnbunny.devword.domain.user.dto.UserSignUpDto;
+import com.burnbunny.devword.domain.user.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public void signUp(UserSignUpDto userSignUpDto) throws Exception {
+        if (userRepository.existsByEmail(userSignUpDto.getEmail())) {
+            throw new Exception("이미 존재하는 이메일입니다.");
+        }
+
+        if (userRepository.existsByNickname(userSignUpDto.getNickname())) {
+            throw new Exception("이미 존재하는 닉네임입니다.");
+        }
+
+        User newUser = User.builder()
+                .email(userSignUpDto.getEmail())
+                .password(userSignUpDto.getPassword())
+                .nickname(userSignUpDto.getNickname())
+                .role(Role.USER)
+                .build();
+
+        newUser.passwordEncode(passwordEncoder);
+        userRepository.save(newUser);
+    }
+}
