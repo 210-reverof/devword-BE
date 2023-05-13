bc4ff237a5c612a119d0ddee026ca54b7bd78a33
given-dragon <yijy001@gmail.com>
2023-05-13 19:20:06 +0900

    feat: update security config #3

--- src/main/java/com/burnbunny/devword/global/config/SecurityConfig.java
+++ src/main/java/com/burnbunny/devword/global/config/SecurityConfig.java
@@ -1,6 +1,10 @@
 package com.burnbunny.devword.global.config;
 
+import com.burnbunny.devword.global.signin.filter.JsonUsernamePasswordAuthenticationFilter;
+import com.burnbunny.devword.global.signin.handler.SigninFailureHandler;
+import com.burnbunny.devword.global.signin.handler.SigninSuccessHandler;
 import com.burnbunny.devword.global.signin.service.SigninService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,7 +24,11 @@ import org.springframework.security.web.authentication.logout.LogoutFilter;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final ObjectMapper objectMapper;
     private final SigninService signinService;
+
+    private final SigninSuccessHandler signinSuccessHandler;
+    private final SigninFailureHandler signinFailureHandler;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -39,6 +47,8 @@ public class SecurityConfig {
                 .requestMatchers("/user/sign-up", "user/check/**").permitAll()
                 .anyRequest().authenticated();
 
+        http.addFilterAfter(jsonUsernamePasswordAuthenticationFilter(), LogoutFilter.class);
+
         return http.build();
     }
 
@@ -53,5 +63,14 @@ public class SecurityConfig {
         provider.setPasswordEncoder(passwordEncoder());
         provider.setUserDetailsService(signinService);
         return new ProviderManager(provider);
+    }
+
+    @Bean
+    public JsonUsernamePasswordAuthenticationFilter jsonUsernamePasswordAuthenticationFilter() {
+        JsonUsernamePasswordAuthenticationFilter signinFilter = new JsonUsernamePasswordAuthenticationFilter(objectMapper);
+        signinFilter.setAuthenticationManager(authenticationManager());
+        signinFilter.setAuthenticationSuccessHandler(signinSuccessHandler);
+        signinFilter.setAuthenticationFailureHandler(signinFailureHandler);
+        return signinFilter;
     }
 }
