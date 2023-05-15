d5e307035d0e2750348cb850dfc52f2bb5a6935e
given-dragon <yijy001@gmail.com>
2023-05-15 15:11:02 +0900

    feat: add send tokens to client when signin success #2

--- src/main/java/com/burnbunny/devword/global/signin/handler/SigninSuccessHandler.java
+++ src/main/java/com/burnbunny/devword/global/signin/handler/SigninSuccessHandler.java
@@ -1,5 +1,6 @@
 package com.burnbunny.devword.global.signin.handler;
 
+import com.burnbunny.devword.global.jwt.service.JwtService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -14,18 +15,30 @@ import java.io.IOException;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class SigninSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final JwtService jwtService;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException, ServletException {
+
         String email = extractUserEmail(authentication);
+        issueAndSendTokens(response, email);
 
         log.info("로그인 성공.");
         log.info("로그인 데이터: ");
         log.info("email: {}", email);
 
         // TODO: 2023/05/13 응답 수정하기
+    }
+
+    private void issueAndSendTokens(HttpServletResponse response, String email) {
+        String accessToken = jwtService.createAccessToken(email);
+        String refreshToken = jwtService.createRefreshToken();
+        jwtService.updateRefreshToken(email, refreshToken);
+        jwtService.sendAccessAndRefreshTokenInResponse(response, accessToken, refreshToken);
     }
 
     private String extractUserEmail(Authentication authentication) {
