a06d7dde534ba32e2955e5b834299d2062361cb1
given-dragon <yijy001@gmail.com>
2023-05-17 22:31:17 +0900

    tmp: refresh token entity id change

--- src/main/java/com/burnbunny/devword/global/jwt/RefreshTokenEntity.java
+++ src/main/java/com/burnbunny/devword/global/jwt/RefreshTokenEntity.java
@@ -12,8 +12,8 @@ import org.springframework.data.redis.core.index.Indexed;
 @Builder
 public class RefreshTokenEntity {
     @Id
-    private String email;
+    private String refreshToken;
 
     @Indexed
-    private String refreshToken;
+    private String email;
 }
